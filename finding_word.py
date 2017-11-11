f0246302b712df4d3eb65a36753d6cb01e5b0462
Trent Bitterman <trentbitterman@comcast.net>
2017-11-10 23:24:03 -0700

    Adds files with helper methods

---
+++ finding_word.py
@@ -0,0 +1,11 @@
+"""finding_word.py - functions to narrow down word_list"""
+
+def narrow_by_length(word_list, word_length):
+    """
+    narrows down word list by removing words
+    of the wrong length
+    """
+
+    for word in word_list:
+        if len(word) is not word_length:
+            word_list.remove(word)
---
+++ initialize_game.py
@@ -0,0 +1,31 @@
+"""initialize_game.py - functions for setting up game state"""
+
+def get_word_length():
+    """Gets the length of the word from the player"""
+
+    # Get game information from player
+    print("How long is your word?")
+
+    # Safely get an int from the player
+    word_length = None
+    while word_length is None:
+        try:
+            word_length = int(input())
+        except ValueError:
+            print("Sorry, you have to enter an integer, e.g. \"5\"")
+
+    return word_length
+
+def initialize_word_list():
+    """Initializes word list"""
+
+    # Get word_file
+    word_file = open("Dictionary.txt", "r")
+
+    word_list = []
+
+    # Iterate over word_file and build list
+    for word in word_file:
+        word_list.append(word.strip)
+
+    return word_list
