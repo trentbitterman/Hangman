3a21b5021cc1c959fdf03714e7f6350996361b60
Trent Bitterman <trentbitterman@comcast.net>
2017-11-11 12:36:34 -0700

    Simplified difficulty code

--- initialize_game.py
+++ initialize_game.py
@@ -38,27 +38,33 @@ def initialize_word_list(filename):
 def get_game_difficulty():
     """Lets player select game difficulty"""
 
-    print(
-        """
-        Would you like to play the game at easy, medium, or hard?
-        Note: Playing the game at hard will heavily slow things down.
-        """)
+    print("Would you like to play the game at easy, medium, or hard?\nNote: Playing the game at hard will heavily slow things down.")
 
     # Ask player for game difficulty
     game_difficulty = None
-    while game_difficulty is not ("easy" or "e" or "medium" or "m" or "hard" or "h"):
+    while game_difficulty is None:
         try:
             game_difficulty = input().lower()
+            if game_difficulty == "easy" or game_difficulty == "e":
+                game_difficulty = "e"
+                break
+            elif game_difficulty == "medium" or game_difficulty == "m":
+                game_difficulty = "m"
+                break
+            elif game_difficulty == "hard" or game_difficulty == "h":
+                game_difficulty = "h"
+                break
+            game_difficulty = None
+            print("Try inputting \"easy\" or \"e\"")
         except ValueError:
             print("Try inputting \"easy\" or \"e\"")
-
     return game_difficulty
 
 def get_game_file(game_difficulty):
     """Returns appropriate filename for set difficulty"""
 
-    if game_difficulty == "hard" or "h":
+    if game_difficulty == "h":
         return "resources/word_list_large.txt"
-    elif game_difficulty == "medium" or "m":
+    elif game_difficulty == "m":
         return "resources/word_list_medium.txt"
     return "resources/word_list_small.txt"
