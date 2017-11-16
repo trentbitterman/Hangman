294debd2d74c0f120b9739c6a33ec5d222a9fad3
Trent Bitterman <trentbitterman@comcast.net>
2017-11-15 22:08:23 -0700

    Added game logic to main method

--- hangman.py
+++ hangman.py
@@ -11,8 +11,52 @@ def main():
     code and the game loop
     """
 
+    game_finished = False
+    guess_letter = "e"
+    hangman_state = 0
+    guessed_letters = ["e"]
+
     # Initialize all game data
     word_list, word_length = ig.initialize_game()
+
+    while not game_finished:
+        # Computer makes guess (first guess always 'e')
+        fw.make_guess(guess_letter)
+        answer = fw.get_answer()
+        if answer:
+            guess_string = fw.get_guess_string()
+            positions = fw.get_location(guess_string, guess_letter)
+            word_list = fw.cull_word_list_by_position(word_list, positions, guess_letter)
+        else:
+            hangman_state += 1
+            ph.print_hangman(hangman_state)
+            word_list = fw.cull_word_list_by_letter(word_list, guess_letter)
+
+        if hangman_state == 6:
+            print("Congratulations, you beat me!")
+            game_finished = True
+            break
+
+        if len(word_list) == 1:
+            print("I think that the word is " + word_list[0] + ". Is that correct?")
+            user_answer = None
+            while user_answer is None:
+                try:
+                    user_answer = input().lower()
+                    if user_answer[0] == "y":
+                        print("Haha, I won!")
+                        game_finished = True
+                        break
+                except ValueError:
+                    print("Try yes or no")
+            else:
+                print("Congratulations, you beat me!")
+                game_finished = True
+
+        guess_letter, guessed_letters = fw.make_new_guess(word_list, guessed_letters)
+
+
+
 
 
 
