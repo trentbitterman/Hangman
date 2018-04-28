89a7ec24940fa1b124ba7b9de3e988a7630892e0
Trent Bitterman <trentbitterman@comcast.net>
2018-04-28 14:09:34 -0600

    Made some new word lists

--- resources/make_lists.py
+++ resources/make_lists.py
@@ -1,4 +1,5 @@
-word_file = open("/Users/trentbitterman/hangman/resources/words_alpha.txt", "r")
+word_file = open(
+    "/Users/trentbitterman/python/hangman/resources/all_words.txt", "r")
 
 word_list = []
 
@@ -9,7 +10,8 @@ word_file.close()
 for i in range(1, 100):
     small_list = [word for word in word_list if len(word) == i]
     if not len(small_list) is 0:
-        filename = "/Users/trentbitterman/hangman/resources/new_lists/" + str(i) + "-words.txt"
+        filename = "/Users/trentbitterman/python/hangman/resources/word_lists/" + \
+            str(i) + "-words.txt"
         new_file = open(filename, "w")
         for word in small_list:
             new_file.write("%s\n" % word)
---
+++ resources/remove_dupes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+words = set()
+
+word_file = open(
+    "/Users/trentbitterman/python/hangman/resources/all_words.txt", "r")
+
+for word in word_file:
+    words.add(word)
+
+words = sorted(words)
+
+word_file.close()
+
+new_file = open(
+    "/Users/trentbitterman/python/hangman/resources/all_words.txt", "w")
+
+for word in words:
+    new_file.write("%s" % word)
+
+new_file.close()
